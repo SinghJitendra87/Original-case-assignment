9056df16ddd3ff69193ffaca5d21325642ecf776
SinghJitendra87 <48727406+SinghJitendra87@users.noreply.github.com>
2019-05-14 06:36:34 -0700

    removed

--- original-case/src/main/java/com/afkl/cases/df/service/AirportService.java
+++
@@ -1,9 +0,0 @@
-package com.afkl.cases.df.service;
-
-import java.util.concurrent.Callable;
-
-import com.afkl.cases.df.model.Locations;
-
-public interface AirportService {
-	public Callable<Locations> getLocations(final String lang);
-}
--- original-case/src/main/java/com/afkl/cases/df/service/MetricService.java
+++
@@ -1,8 +0,0 @@
-package com.afkl.cases.df.service;
-
-import com.afkl.cases.df.model.StatisticsMetric;
-
-public interface MetricService {
-	public void increaseCount(int status);
-	public StatisticsMetric getStatusMetric();
-}
