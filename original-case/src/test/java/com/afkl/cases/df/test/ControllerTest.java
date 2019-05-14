cd33ed3b05ea018554d28b3d15fd7505a281678b
singh <singh@192.168.178.17>
2019-05-14 10:38:01 -0700

    Junit test cases

---
+++ original-case/src/test/java/com/afkl/cases/df/test/ControllerTest.java
@@ -0,0 +1,62 @@
+package com.afkl.cases.df.test;
+
+import static org.junit.Assert.assertThat;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.web.client.RestTemplate;
+
+import com.afkl.cases.df.model.FareDetails;
+import com.afkl.cases.df.model.Locations;
+
+@SpringBootTest
+@ActiveProfiles("test")
+@RunWith(SpringJUnit4ClassRunner.class)
+public class ControllerTest {
+
+	@Value("${original.uri.fareDetails}")
+	private String uriFairDetails;
+	
+	
+	
+	@Value("${original.uri.airportsAll}")
+	private String uriairportsAll;
+
+	@Test
+	public void getLocationsTest() {
+
+		final RestTemplate restTemplate = new RestTemplate();
+		ResponseEntity<Locations> response = restTemplate.getForEntity(uriairportsAll, Locations.class);
+
+		Assert.assertNotNull(response);
+		Assert.assertEquals(response.getStatusCode(), HttpStatus.OK);
+
+	}
+
+	@Test
+	public void getFairDetailsTest() {
+
+		final RestTemplate restTemplate = new RestTemplate();
+
+		final Map<String, String> params = new HashMap<String, String>();
+		params.put("origin", "AAL");
+		params.put("destination", "AOJ");
+
+		final FareDetails fareDetails = restTemplate.getForObject(uriFairDetails, FareDetails.class, params);
+		Assert.assertEquals("Aalborg - Aalborg (AAL), Denmark", fareDetails.getOriginDescription());
+
+	}
+
+}
